78263e2df1e2a2cb7c517d1db6b6559fac9a54ab
Pierce <pierceh.dev@gmail.com>
2018-03-13 13:48:36 -0500

    support for active support 4.0.13

--- lib/rails-callback_log.rb
+++ lib/rails-callback_log.rb
@@ -24,21 +24,6 @@ module RailsCallbackLog
     end
   end
 
-  # In rails 5.1, we extend `CallTemplate`.
-  module CallTemplateExtension
-    # Returns a lambda that wraps `super`, adding logging.
-    def make_lambda
-      original_lambda = super
-      lambda { |*args, &block|
-        if !::RailsCallbackLog::FILTER ||
-          caller.any? { |line| ::RailsCallbackLog.matches_filter?(line) }
-          ::RailsCallbackLog.logger.debug(format("Callback: %s", @method_name))
-        end
-        original_lambda.call(*args, &block)
-      }
-    end
-  end
-
   # In rails 4.2 and 5.0, we extend `Callback`.
   module CallbackExtension
     # Returns a lambda that wraps `super`, adding logging.
@@ -58,14 +43,8 @@ end
 # Install our `CallbackExtension` using module prepend.
 module ActiveSupport
   module Callbacks
-    if ::ActiveSupport.gem_version >= ::Gem::Version.new("5.1.0")
-      class CallTemplate
-        prepend ::RailsCallbackLog::CallTemplateExtension
-      end
-    else
-      class Callback
-        prepend ::RailsCallbackLog::CallbackExtension
-      end
+    class Callback
+      prepend ::RailsCallbackLog::CallbackExtension
     end
   end
 end
